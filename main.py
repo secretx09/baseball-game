4894f3c2e0536517318ea3a6a9846afc1f2bfc75
secretx09 <freedomfighter3986@gmail.com>
2025-05-05 11:32:18 -0400

    made it less lines

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ class Game:
 
 
 class Score: 
-    home_hits = 0
-    away_hits = 0
-    home_runs = 0
-    away_runs = 0
+    home_hits, away_hits, home_runs, away_runs = 0
+
+def roll(n=2, d_side=6):
+    return [random.randint(1,d_side) for i in range(n)]
+
+r = roll(d_side=100)
