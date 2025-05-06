7f64dd922b0c3bdc2526b1ba1b5c71449f79ef62
secretx09 <freedomfighter3986@gmail.com>
2025-05-06 10:02:08 -0400

    reformatted all of the code

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import random
 
+class Score: 
+    home_hits, away_hits, home_runs, away_runs = 0, 0, 0, 0
+
 class Game:
+    def roll():
+        return [random.randint(1, 6) for i in range(2)] #ex: [4,2]
+    
+    r = roll()
+
     outs = 0
     inning = 0
-
-
-class Score: 
-    home_hits, away_hits, home_runs, away_runs = 0
-
-def roll(n=2, d_side=6):
-    return [random.randint(1,d_side) for i in range(n)]
-
-r = roll(d_side=100)
