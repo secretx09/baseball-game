5e06a892286f3d7d82518c845631d6571297a4c7
secretx09 <101412985+secretx09@users.noreply.github.com>
2025-05-06 18:57:46 +0000

    added different scenarios for dice combinations

--- main.py
+++ main.py
@@ -11,6 +11,36 @@ class Game:
 
     outs = 0
     inning = 0
+
+    if r == [1, 1] or r == [6, 6]:
+        print("Homerun")
+
+    elif r == [1, 2] or r == [2, 1] or r == [5, 5]:
+        print("Double")
+
+    elif r == [1, 3] or r == [3, 1] or r == [3, 6] or r == [6, 3]:
+        print("Fly Out")
+
+    elif r == [1, 4] or r == [4, 1] or r == [3, 3] or r == [4, 4]:
+        print("Walk")
+
+    elif r == [1, 5] or r == [5, 1] or r == [4, 5] or r == [5, 4]:
+        print("Pop Out")
+
+    elif r == [1, 6] or r == [6, 1] or r == [2, 5] or r == [5, 2]:
+        print("Single")
+
+    elif r == [2, 2]:
+        print("Double Play")
+
+    elif r == [2, 3] or r == [3, 2] or r == [3, 5] or r == [5, 3]:
+        print("Groundout")
     
-    if r == [1, 5]:
-        print("test success")
+    elif r == [2, 4] or r == [4, 2] or r == [2, 6] or r == [6, 2] or r == [4, 6] or r == [6, 4]:
+        print("Strikeout")
+
+    elif r == [3, 4] or r == [4, 3]:
+        print("Triple")
+
+    elif r == [5, 6] or r == [6, 5]:
+        print("Sacrifice Fly")
